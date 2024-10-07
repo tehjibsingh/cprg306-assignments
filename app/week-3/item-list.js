31a8446d1cbb877ce16c829a6dcde1aa7b1af736
tehjibsingh <tehjibsingh@edu.sait.ca>
2024-10-07 16:04:17 -0600

    graphics in week 3

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,3 +1,5 @@
+// /app/week-3/item-list.js
+
 import Item from './item';
 
 export default function ItemList() {
@@ -17,7 +19,7 @@ export default function ItemList() {
   ];
 
   return (
-    <ul className="space-y-4">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {items.map((item, index) => (
         <Item
           key={index}
